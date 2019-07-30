a8b93b6bb9ae2e0eebd8195d985939737e1e36c6
Lauren Cappiello <lgpcappiello@gmail.com>
2019-07-30 14:58:51 -0700

    Create wordcloud.R

---
+++ teaching/stat100a/wordcloud.R
@@ -0,0 +1,44 @@
+#install packages
+install.packages("tm")
+install.packages("SnowballC")
+install.packages("wordcloud")
+install.packages("RColorBrewer")
+
+#load packages
+library(tm)
+library(SnowballC)
+library(wordcloud)
+library(RColorBrewer)
+
+#read in text file of interest (this is the only thing you need to change!)
+text <- readLines('/Users/cappiello/Documents/stat100a/dragon.txt')
+
+docs <- Corpus(VectorSource(text))
+inspect(docs)
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+# Convert the text to upper case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+docs <- tm_map(docs, stemDocument)
+
+#create frequency table
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+#print top 10 words
+head(d, 10)
+
+#create word cloud
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1, scale = c(4,1),
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
